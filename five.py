b6193883a848916ad6aed0b1ac88b2a67d20d1c9
Tim van Boxtel <tim@vanboxtel.ca>
2019-12-06 15:53:49 -0500

    Day 5B solution complete

--- five.py
+++ five.py
@@ -1,6 +1,7 @@
 import os
 
 in_file = os.path.join(os.getcwd(), 'input', 'five.txt')
+out_file = os.path.join(os.getcwd(), 'five_out_1.txt')
 
 assert os.path.exists(in_file), "Expecting input file: {}".format(in_file)
 
@@ -25,63 +26,119 @@ OPCODES are coded as 4 or 5 bit values: The last 2 digits are the opcode, and th
 """
 DEBUG = True
 
-def get_val(mem, p, mode):
+def debug(msg):
+    if DEBUG:
+        print("DEBUG -- " + msg)
+        #with open(out_file, 'w') as f:
+        #    f.write("DEBUG " + msg + os.linesep)
+
+def get_val(mem: list, idx: int, mode: int = 0):
     if mode:
-        return p
+        # Immediate
+        return mem[idx]
     else:
-        if p < 0 :
+        # positional
+        if idx < 0 :
             raise ValueError("Value of positional mode can't be negative")
-        return mem[p]
+        return mem[mem[idx]]
 
 # OPCODE: 01
-def add(mem: list, p1: int, p2: int, p3: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
-    v2 = get_val(mem, p2, mode_flag & 0b010)
-    mem[p3] = v1 + v2
-    
+def add_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    v2 = get_val(mem, idx + 2, modeflag & 0b010)
+    loc = get_val(mem, idx + 3, True)
+
+    debug("{} ADD {} {} {}".format(idx, v1, v2, loc))
+
+    mem[loc] = v1 + v2
+    return idx + 4
+
 # OPCODE: 02
-def multiply(mem: list, p1: int, p2: int, p3: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
-    v2 = get_val(mem, p2, mode_flag & 0b010)
-    mem[p3] = v1 * v2
+def multiply_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    v2 = get_val(mem, idx + 2, modeflag & 0b010)
+    loc = get_val(mem, idx + 3, True)
+
+    debug("{} MULT {} {} {}".format(idx, v1, v2, loc))
+
+    mem[loc] = v1 * v2
+    return idx + 4
 
 # OPCODE: 03
-def op_input(mem: list, p1: int, mode_flag=0b000):
-    # Get input?
-    v1 = int(input("Provide Integer Input: "))
-    mem[p1] = v1
+def op_input_x(mem: list, idx: int, modeflag: int):
+    loc = get_val(mem, idx + 1, True)
+    debug("{} INPUT {}".format(idx, loc))
+    v1 = int(input("Enter Input: "))
+    mem[loc] = v1
+    return idx + 2
 
 # OPCODE: 04
-def op_output(mem: list, p1: int, mode_flag=0b000):
-    # Return output?
-    v1 = get_val(mem, p1, mode_flag & 0b001)
-    print("Output: ", v1)
+def op_output_x(mem: list, idx: int, modeflag: int):
+    val = get_val(mem, idx + 1, modeflag & 0b001)
+    debug("{} OUTPUT {}".format(idx, val))
+    print("-- PROGRAM OUTPUT: ", val, " -- ")
+    return idx + 2
 
 # OPCODE: 05
-def jump_if_true(mem: list, p1:int, p2: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
+def jit_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    loc = get_val(mem, idx + 2, modeflag & 0b010)
+
+    debug("{} JIT {} {}".format(idx, v1, loc))
+
     if v1 != 0:
-        return p2
-    return None
+        idx = loc
+    else:
+        idx += 3
+    return idx
 
 # OPCODE: 06
-def jump_if_false(mem: list, p1: int, p2: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
+def jif_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    loc = get_val(mem, idx + 2, modeflag & 0b010)
+
+    debug("{} JIF {} {}".format(idx, v1, loc))
+
     if v1 == 0:
-        return p2
-    return None
+        idx = loc
+    else:
+        idx += 3
+    return idx
 
 # OPCODE: 07
-def is_less_than(mem: list, p1: int, p2: int, p3: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
-    v2 = get_val(mem, p2, mode_flag & 0b010)
-    mem[p3] =  int(v1 < v2)
+def is_lt_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    v2 = get_val(mem, idx + 2, modeflag & 0b010)
+    loc = get_val(mem, idx + 3, True)
+
+    debug("{} ISLT {} {} {}".format(idx, v1, v2, loc))
+
+    mem[loc] = int(v1 < v2)
         
+    return idx + 4
+
 # OPCODE: 08
-def is_equals(mem: list, p1: int, p2: int, p3: int, mode_flag=0b000):
-    v1 = get_val(mem, p1, mode_flag & 0b001)
-    v2 = get_val(mem, p2, mode_flag & 0b010)
-    mem[p3] = int(v1 == v2)
+def is_eq_x(mem: list, idx: int, modeflag: int):
+    v1 = get_val(mem, idx + 1, modeflag & 0b001)
+    v2 = get_val(mem, idx + 2, modeflag & 0b010)
+    loc = get_val(mem, idx + 3, True)
+
+    debug("{} ISEQ {} {} {}".format(idx, v1, v2, loc))
+
+    mem[loc] = int(v1 == v2)
+
+    return idx + 4
+
+op_codes = {
+    1: add_x,
+    2: multiply_x,
+    3: op_input_x,
+    4: op_output_x,
+    5: jit_x,
+    6: jif_x,
+    7: is_lt_x,
+    8: is_eq_x
+}
 
 def parse_opcode(v: int):
     sv = str(v)
@@ -90,79 +147,24 @@ def parse_opcode(v: int):
     else:
         return int(sv[-2:]), int(sv[:-2])
 
-def debug(msg):
-    if DEBUG:
-        print("D-- " + msg)
-
 def run_program(program: list):
     mem = program.copy()
     debug("Program size: {}".format(len(mem)))
     idx = 0
     while idx < len(mem):
+        # debug(str(mem))
         opcode, modeflag = parse_opcode(mem[idx])
-        # debug("OP_MODE: {} {} | {}".format(idx, opcode, modeflag))
-        if opcode == 1:
-            p1, p2, p3 = mem[idx+1:idx+4]
-            debug("{} {} | ADD {} {} -> {}".format(idx, mem[idx:idx+4], p1, p2, p3))
-            add(mem, p1, p2, p3, modeflag)
-            idx += 4
+        if opcode == 99:
+            print("Program Halt")
+            return mem
 
-        elif opcode == 2:
-            p1, p2, p3 = mem[idx+1:idx+4]
-            debug("{} {} | MULT {} {} -> {}".format(idx, mem[idx:idx+4], p1, p2, p3))
-            multiply(mem, p1, p2, p3, modeflag)
-            idx += 4
-
-        elif opcode == 3:
-            p1 = mem[idx+1]
-            debug("{} {} | INP -> {}".format(idx, mem[idx:idx+2], p1))
-            op_input(mem, p1)
-            idx += 2
-
-        elif opcode == 4:
-            p1 = mem[idx + 1]
-            debug("{} {} | OUTP {}".format(idx, mem[idx:idx+2], p1))
-            op_output(mem, p1, modeflag)
-            idx += 2
-
-        elif opcode == 5:
-            p1, p2 = mem[idx + 1: idx+3]
-            debug("{} {} | JIT {} -> {}".format(idx, mem[idx:idx+3], p1, p2))
-            ret = jump_if_true(mem, p1, p2, modeflag)
-            if ret is not None:
-                idx = ret
-            else:
-                idx += 3
-
-        elif opcode == 6:
-            p1, p2 = mem[idx+1: idx + 3]
-            debug("{} {} | JIF {} -> {}".format(idx, mem[idx:idx+3], p1, p2))
-            ret = jump_if_false(mem, p1, p2, modeflag)
-            if ret is not None:
-                idx = ret
-            else:
-                idx += 3
-
-        elif opcode == 7:
-            p1, p2, p3 = mem[idx + 1: idx + 4]
-            debug("{} {} | ISLT {} {} -> {}".format(idx, mem[idx:idx+4], p1, p2, p3))
-            is_less_than(mem, p1, p2, p3, modeflag)
-            idx += 4
-
-        elif opcode == 8:
-            p1, p2, p3 = mem[idx + 1: idx + 4]
-            debug("{} {} | ISEQ {} {} -> {}".format(idx, mem[idx:idx+4], p1, p2, p3))
-            is_equals(mem, p1, p2, p3, modeflag)
-            idx += 4
-
-        elif opcode == 99:
-            debug("{} {} | BREAK".format(idx, mem[idx]))
-            break
-        
+        try:
+            operation = op_codes[opcode]
+        except IndexError:
+            print("Bad OP Code: {}.  Exiting".format(opcode))
+            return mem
         else:
-            print("{} Bad opcode: {}. Exiting".format(idx, opcode))
-            break
-    return mem
+            idx = operation(mem, idx, modeflag)
 
 def test1():
     r1 =  run_program([1101, 100, -1, 4, 0])
@@ -172,17 +174,17 @@ def test1():
 # test1()
 
 def test2():
-    r1 = run_program([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
-    # run_program([3,9,8,9,10,9,4,9,99,-1,8])
+    run_program([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+    # run_program([3,9,8,9,10,9,4,9,99,-1,8])  # Checks whether input is equal to 8, returns 1, returns 0 otherwise 
 # test2()
 
 def test3():
-    # run_program([3,9,8,9,10,9,4,9,99,-1,8])  # Output 1 if input is equal to 8, 0 if not
+    # print(run_program([3,9,8,9,10,9,4,9,99,-1,8]))  # Output 1 if input is equal to 8, 0 if not
     # run_program([3,9,7,9,10,9,4,9,99,-1,8])  # Output 1 if input is less than 8, 0 otherwise
     # run_program([3,3,1108,-1,8,3,4,3,99])  # Output 1 is input is equalt to 8, using immediate mode
     # run_program([3,3,1107,-1,8,3,4,3,99])  # Output 1 if input is less than 8, 0 otherwise using immediate mode
     
-    print(run_program([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]))  # Output 0 if the input is 0, 1 otherwise
+    run_program([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])  # Output 0 if the input is 0, 1 otherwise
     # This is broken when evaluating input of 0.  But why?
     # It seems as though the code is broken, specifically at memory location 2: [6, 12, 15].  Jump if False, position 12 to position 15.  Position 12 is the input provide
     # Thus if a zero is provided as input, it wants to jump to position 15, which is the last entry or '9'.  This is a bad opcode and no output ensues...  
@@ -190,8 +192,8 @@ def test3():
 
     # run_program([3,3,1105,-1,9,1101,0,0,12,4,12,99,1])  # Same as above, with immediate mode
 
-    # run_program([1107,1,0,7,4,7,99,1])
-test3()
+    # print(run_program([1107,1,0,7,4,7,99,1]))
+# test3()
 
 def go():
     # Read program
@@ -200,5 +202,5 @@ def go():
 
     run_program(mem)
 
-# go()
+go()
 
