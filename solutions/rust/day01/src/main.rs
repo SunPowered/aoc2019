7db44ea18c575a097f241c662d2b1a3c5efdce9b
Tim van Boxtel <tim@vanboxtel.ca>
2019-12-08 14:14:54 -0500

    rust: day 01 complete

--- solutions/rust/day01/src/main.rs
+++ solutions/rust/day01/src/main.rs
@@ -1,19 +1,31 @@
 use std::fs;
-fn fuel_required(mass: u32) -> Option<u32> {
-	return (mass / 3).checked_sub(2);
+fn fuel_required(&mass: &i32) -> i32 {
+	return (&mass / 3).checked_sub(2).unwrap();
 }
 
+
+fn recursive_fuel(&mass: &i32) -> i32 {
+
+	let mut res: i32 = fuel_required(&mass);
+	let mut total: i32 = 0;
+	while res > 0 {
+		// println!("Iter: {}. Total: {}", res, total);
+		total += res;
+		res = fuel_required(&res);
+	}
+	total
+}
 
 fn part1() {
 
 	println!("Part 1");
     let contents = fs::read_to_string("input.txt").expect("Unable to parse file");
-    let mut total: u32 = 0;
+    let mut total: i32 = 0;
     let mut count: usize = 0;
     
     for line in contents.lines() {
-    	let val: u32 = line.trim().parse().unwrap();
-    	let res: u32 = fuel_required(val).unwrap();
+    	let val: i32 = line.trim().parse().unwrap();
+    	let res: i32 = fuel_required(&val);
     	total += res;
     	count += 1;
     }
@@ -25,9 +37,21 @@ fn part1() {
 
 fn part2() {
 	println!("Part 2");
-	
-}
+	let contents = fs::read_to_string("input.txt").expect("Unable to parse file");
+    let mut total: i32 = 0;
+    let mut count: usize = 0;
+    
+    for line in contents.lines() {
+    	let val: i32 = line.trim().parse().unwrap();
+    	let res: i32 = recursive_fuel(&val);
+    	total += res;
+    	count += 1;
+    }
 
+    println!("Count: {}. Total Fuel Required: {}", count, total);
+    // Count: 100. Total Fuel Required: 4946546
+
+}
 
 fn main() {
 	part1();
@@ -40,9 +64,16 @@ mod tests {
 
 	#[test]
 	fn fuel_tests(){
-		assert_eq!(fuel_required(12).unwrap(), 2);
-		assert_eq!(fuel_required(14).unwrap(), 2);
-		assert_eq!(fuel_required(1969).unwrap(), 654);
-		assert_eq!(fuel_required(100756).unwrap(), 33583);
+		assert_eq!(fuel_required(&12), 2);
+		assert_eq!(fuel_required(&14), 2);
+		assert_eq!(fuel_required(&1969), 654);
+		assert_eq!(fuel_required(&100756), 33583);
+		assert_eq!(fuel_required(&5), -1);
+	}
+	#[test]
+	fn part2_tests(){
+		assert_eq!(recursive_fuel(&14), 2);
+		assert_eq!(recursive_fuel(&1969), 966);
+		assert_eq!(recursive_fuel(&100756), 50346);
 	}
 }
